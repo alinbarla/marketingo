fd6a7bef01c8eb793b18cc28d9a78e70dd805b6e
Eduardo Valencia <8025918@gmail.com>
2020-12-26 19:54:39 -0800

    Add call to action

---
+++ packages/f1/src/components/Home/CallToAction/index.js
@@ -0,0 +1,109 @@
+import React, { Component } from "react";
+import { styled } from "frontity";
+
+import sectionImage from "./negocio-online-estrategias-marketing.svg";
+import breakpoints from "../../../constants/breakpoints";
+
+const col1 = "col1";
+
+const col2 = "col2";
+
+const Container = styled.div`
+  padding: 8% 5%;
+  background-color: #f6f5f1;
+  box-sizing: border-box;
+  background-image: url(${sectionImage});
+  background-repeat: no-repeat;
+  width: 100%;
+`;
+
+const flexBreakpoint = breakpoints.md;
+const Grid = styled.div`
+  max-width: 43.75rem;
+  margin: 0 auto;
+  display: grid;
+  column-gap: 4rem;
+  font-weight: 500;
+  grid-template-areas: "${col1}" "${col2}";
+  grid-template-columns: 18rem;
+  grid-template-rows: 13.6992rem 14.2617rem;
+
+  @media (min-width: ${flexBreakpoint}) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+    grid-gap: 0 4em;
+  }
+`;
+
+const ImageColumn = styled.div`
+  grid-area: ${col1};
+  padding-bottom: 1.2rem;
+  width: 100%;
+`;
+
+const Image = styled.img`
+  @media (min-width: ${flexBreakpoint}) {
+    width: 46.875rem;
+    height: auto;
+    display: none;
+  }
+`;
+
+const BodyColumn = styled.div`
+  grid-area: ${col2};
+  margin-top: 2.5rem;
+  align-self: center;
+
+  @media (min-width: ${flexBreakpoint}) {
+    margin-left: 9.375rem;
+    grid-area: none;
+  }
+`;
+
+const Title = styled.h2`
+  margin-bottom: 1.25rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media (min-width: ${breakpoints.lg}) {
+    justify-content: flex-end;
+  }
+`;
+
+const Link = styled.a`
+  color: #ffffff;
+  background-color: #00d07e;
+  transition: transform 0.2s ease !important;
+  border-radius: 6.25rem;
+  padding: 0.75rem 1.25rem;
+  font-weight: 700;
+  font-size: 1.25rem;
+`;
+
+export class CallToAction extends Component {
+  render() {
+    return (
+      <Container>
+        <Grid>
+          <ImageColumn>
+            <Image
+              src={sectionImage}
+              alt="Woman holding a search bar. It has lines connecting to a home and shopping icon underneath it. Also, it has a cursor indicating that someone clicked on the search bar."
+            />
+          </ImageColumn>
+          <BodyColumn>
+            <Title>Las mejores estrategias marketing!</Title>
+            <ButtonContainer>
+              <Link href="#">Todos los recursos</Link>
+            </ButtonContainer>
+          </BodyColumn>
+        </Grid>
+      </Container>
+    );
+  }
+}
+
+export default CallToAction;
--- packages/f1/src/components/Home/index.js
+++ packages/f1/src/components/Home/index.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
+import { styled } from "frontity";
 
 import Hero from "./Hero";
 import Testimonials from "./Testimonials";
 import Introduction from "./Introduction";
 import Transition from "./Transition";
+import CallToAction from "./CallToAction";
+
+const Container = styled.div`
+  width: 100%;
+`;
 
 export class Home extends Component {
   render() {
     return (
-      <div>
+      <Container>
         <Hero />
         <Testimonials />
         <Introduction />
         <Transition />
-      </div>
+        <CallToAction />
+      </Container>
     );
   }
 }
