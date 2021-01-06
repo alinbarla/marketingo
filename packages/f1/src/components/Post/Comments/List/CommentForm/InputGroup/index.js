8b6e10942d5568a49215f7b355e28e4286019f2a
Eduardo Valencia <8025918@gmail.com>
2021-01-06 12:50:51 -0800

    Clear form after comment submission

--- packages/f1/src/components/Post/Comments/List/CommentForm/InputGroup/index.js
+++ packages/f1/src/components/Post/Comments/List/CommentForm/InputGroup/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { connect } from "frontity";
 
-const InputGroup = ({ Input, actions, postId, name }) => {
+const InputGroup = ({ Input, actions, state, postId, name }) => {
   const handleChange = (event) => {
     actions.comments.updateFields(postId, {
       [name]: event.target.value,
     });
   };
-  return <Input onChange={handleChange} />;
+
+  const getFormValue = () => {
+    const { [postId]: form } = state.comments.forms;
+    if (form && form.fields) {
+      return form.fields[name] || "";
+    }
+    return "";
+  };
+
+  return <Input onChange={handleChange} value={getFormValue()} />;
 };
 
 export default connect(InputGroup);
