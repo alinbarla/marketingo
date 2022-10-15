a99b6408ff834c543e39c555ed885ee2efb537a5
Alin Barla <alin@marketingo.it>
2022-10-15 17:14:40 +0200

    Update index.js

--- packages/f1/src/components/Blog/Item/index.js
+++ packages/f1/src/components/Blog/Item/index.js
@@ -107,6 +107,7 @@ import { useTheme } from "@material-ui/core/styles";
 
 const StyledCard = styled(Card)`
   margin: 0 auto 3.5rem;
+  border: none !important;
   border-radius: 10px !important;
   box-shadow: 0 100px 80px rgb(70 125 249 / 4%), 0 41.7776px 33.4221px rgb(70 125 249 / 3%), 0 22.3363px 17.869px rgb(70 125 249 / 2%), 0 12.5216px 10.0172px rgb(70 125 249 / 2%), 0 6.6501px 5.32008px rgb(70 125 249 / 2%), 0 2.76726px 2.21381px rgb(70 125 249 / 1%);
 `;
