d994cb9a91aedc5d66555145517b180dca05cc3b
Miracutor <miracleexecutors@gmail.com>
2022-10-22 01:15:31 +0800

    Add shouldForwardProp for StyledLink

--- packages/f1/src/components/Blog/StyledLink.js
+++ packages/f1/src/components/Blog/StyledLink.js
@@ -2,7 +2,9 @@ import React from "react";
 import { styled } from "frontity";
 import Link from "@material-ui/core/Link";
 
-const StyledLink = styled(Link)`
+const StyledLink = styled(Link, {
+  shouldForwardProp: (prop) => prop !== "colorLink",
+})`
   transition: color 250ms !important;
   &:hover {
     color: ${(props) => props.colorLink};
