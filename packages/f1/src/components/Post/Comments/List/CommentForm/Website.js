1426646829f7845bb54f9bd16c476cf31131977a
Alin Barla <alin@marketingo.it>
2022-10-23 16:35:22 +0200

    Update Website.js

--- packages/f1/src/components/Post/Comments/List/CommentForm/Website.js
+++ packages/f1/src/components/Post/Comments/List/CommentForm/Website.js
@@ -3,7 +3,7 @@ import React from "react";
 import InputGroup from "./InputGroup";
 import BaseInput from "./InputGroup/inputs/Input";
 
-const Input = (props) => <BaseInput placeholder="Tu sitio web" {...props} />;
+const Input = (props) => <BaseInput placeholder="Sito web" {...props} />;
 
 const Website = (props) => (
   <InputGroup name="authorURL" Input={Input} {...props} />
