de5a169d60a9df12b336205931fb9dae91f70fcc
Miracutor <miracleexecutors@gmail.com>
2022-10-28 21:26:10 +0800

    Add htmltooltip and icon

--- packages/f1/src/processor/tooltip.js
+++ packages/f1/src/processor/tooltip.js
@@ -1,5 +1,49 @@
 import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { styled } from "frontity";
 import Tooltip from "@material-ui/core/Tooltip";
+import Typography from "@material-ui/core/Typography";
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: "#f5f5f9",
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: "1px solid #dadde9",
+  },
+}))(Tooltip);
+
+const InfoIcon = (props) => (
+  <SvgIcon {...props}>
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path>
+  </SvgIcon>
+);
+
+const TooltipTrigger = React.forwardRef((props, ref) => (
+  <React.Fragment>
+    <span
+      ref={ref}
+      style={{
+        textDecoration: "underline dotted",
+        color: props.color,
+      }}
+      {...props}
+    />
+    <InfoIcon
+      style={{ position: "absolute", fontSize: "1.00rem" }}
+      htmlColor={props.color}
+    />
+  </React.Fragment>
+));
+
+const TitleTooltipsComponent = ({ title, description }) => (
+  <React.Fragment>
+    <Typography color="inherit">{title}</Typography>
+    {description}
+  </React.Fragment>
+);
 
 const tooltips = {
   name: "tooltips",
@@ -10,13 +54,27 @@ const tooltips = {
     node.props?.title,
 
   processor: ({ node }) => {
-    const tooltipTitle = node.props.title;
-    const tooltipContent = node.children[0].content;
+    const tooltipTitle = node.props.title || "";
+    const tooltipDescription = node.props.description || "";
+    const tooltipContent = node.children[0].content || "";
+    const tooltipColor = node.props.color || "inherit";
+
     node.component = () =>
       React.createElement(
-        Tooltip,
-        { title: tooltipTitle, arrow: true },
-        React.createElement("span", null, tooltipContent)
+        HtmlTooltip,
+        {
+          title: React.createElement(
+            TitleTooltipsComponent,
+            { title: tooltipTitle, description: tooltipDescription },
+            []
+          ),
+          arrow: true,
+        },
+        React.createElement(
+          TooltipTrigger,
+          { color: tooltipColor },
+          tooltipContent
+        )
       );
     return node;
   },
